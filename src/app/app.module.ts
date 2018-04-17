5ca7fa9fc16689ebacb7d5d590c0c9d53c49cd94
Mads Jørgensen <mjo@ccieurope.com>
2018-04-17 21:25:59 +0200

    Added basic content to dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FlexLayoutModule } from "@angular/flex-layout";
-import { FormsModule } from "@angular/forms";
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from "./app-routing/app-routing.module";
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { LoginComponent } from './login/login.component';
 import { InputfieldDirective } from './forms/inputfield.directive';
 
-import { AngularFireModule } from "angularfire2";
-import { environment } from "../environments/environment";
-import { AngularFireAuthModule } from "angularfire2/auth";
-import { AngularFireStorageModule } from "angularfire2/storage";
+import { AngularFireModule } from 'angularfire2';
+import { environment } from '../environments/environment';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireStorageModule } from 'angularfire2/storage';
 
-import { AuthService } from "./auth.service";
-import { ImageService } from "./image.service";
+import { AuthService } from './auth.service';
+import { ImageService } from './image.service';
 import { CreateProfileComponent } from './create-profile/create-profile.component';
 import { TopNavigationComponent } from './top-navigation/top-navigation.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
